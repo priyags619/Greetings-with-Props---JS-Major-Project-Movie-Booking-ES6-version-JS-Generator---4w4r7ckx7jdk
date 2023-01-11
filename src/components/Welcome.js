83a611c4688f90fae14c41bce13509666691977b
priyags619 <118427427+priyags619@users.noreply.github.com>
2023-01-11 22:01:32 +0530

    welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,16 @@
 // write code for Welcome component here
+import React from "react";
+
+// write code for Welcome component here
+const Welcome = (props) => {
+  const { name } = props;
+
+  return (
+    <>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+};
+
+export default Welcome;
